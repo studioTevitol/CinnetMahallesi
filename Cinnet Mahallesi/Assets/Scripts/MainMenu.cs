2d69ee88aa808b5457d214645f682bd4413d77a9
theko1 <aecakir@tevitol.k12.tr>
2022-12-08 23:51:18 +0300

    main menu script is added

---
+++ Cinnet Mahallesi/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("House_Messy");
+	}
+
+	public void Credits()
+	{
+		SceneManager.LoadScene("credits_scene");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
