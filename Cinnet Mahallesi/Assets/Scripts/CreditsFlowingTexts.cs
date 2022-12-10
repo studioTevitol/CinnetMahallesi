a68747aa14290af46154beb91a40196f3f7a8c1f
theko1 <aecakir@tevitol.k12.tr>
2022-12-10 16:56:00 +0300

    credits are revised and completed

---
+++ Cinnet Mahallesi/Assets/Scripts/CreditsFlowingTexts.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsFlowingTexts : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (transform.position.y >= 1300)   
+        {
+            Application.Quit();
+        }
+    }
+}
