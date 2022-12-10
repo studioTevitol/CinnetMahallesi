91fb9c6ebfb4603f10408e96496e9fde9c4efd4a
theko1 <aecakir@tevitol.k12.tr>
2022-12-10 18:28:56 +0300

    ending

--- Cinnet Mahallesi/Assets/Scripts/SceneTransis.cs
+++ Cinnet Mahallesi/Assets/Scripts/SceneTransis.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransis : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-		    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	public void OnTriggerEnter2D(Collider2D collision)
+    { 
+	    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
     public void BackToHomeTransis()
