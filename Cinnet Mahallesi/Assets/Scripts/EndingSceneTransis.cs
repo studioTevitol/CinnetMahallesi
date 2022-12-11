532a7ce74943e8f8f17c368f8d8cc44699373cfc
UmutBideci <116887488+UmutBideci@users.noreply.github.com>
2022-12-11 15:06:04 +0300

    resolving merge conflicts #1

---
+++ Cinnet Mahallesi/Assets/Scripts/EndingSceneTransis.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingSceneTransis : MonoBehaviour
+{
+    public CharacterMovement player;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (player.endingNumber == 0)
+        {
+            SceneManager.LoadScene("Cathedral_Good");
+        }
+        else
+        {
+            SceneManager.LoadScene("Cathedral_Chaos");
+        }
+    }
+}
